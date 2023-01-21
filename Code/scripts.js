399bb31f9841e0592ef3dec3064508275e06b1e0
Anuka <arrinsamurai7@gmail.com>
2023-01-21 17:45:15 +0530

    js comp

--- Code/scripts.js
+++ Code/scripts.js
@@ -3,6 +3,8 @@ const qr = document.getElementById('qr-code');
 
 const onGenerateSubmit = (e) => {
     e.preventDefault();
+     
+    clearUi();
     
     const url = document.getElementById('url').value;
     const size = document.getElementById('size').value;
@@ -19,6 +21,12 @@ const onGenerateSubmit = (e) => {
             hideSpinner();
             generateQRCode(url, size);
 
+            setTimeout(() => {
+                const saveUrl = qr.querySelector('img').src;
+                createSaveBtn(saveUrl);
+                
+            }, 50);
+
         }, 1000);
     }
 };
@@ -34,6 +42,23 @@ const generateQRCode = (url, size) => {
 
 const showSpinner = () => document.getElementById('spinner').style.display = 'block';
 const hideSpinner = () => document.getElementById('spinner').style.display = 'none';
+
+const clearUi = () => {
+    qr.innerHTML = '';
+    const saveLink = document.getElementById('save-link');
+    
+    if(saveLink) saveLink.remove();
+}
+
+const createSaveBtn = (saveUrl) => {
+    const link = document.createElement('a');
+    link.id = 'save-link';
+    link.classList = 'bg-red-500 text-white font-bold py-2 rounded w-1/3 m-auto my-s5';
+    link.href = saveUrl;
+    link.download = 'qr-code';
+    link.innerHTML = 'Save Image';
+    document.getElementById('generated').appendChild(link);
+}
 
 hideSpinner();
 
